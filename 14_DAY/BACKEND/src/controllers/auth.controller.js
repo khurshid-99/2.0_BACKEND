5b628f849415577016b10e2e696b42b26f37f86d
Khurshid Alam <khurshid1556@gmail.com>
2026-02-11 15:37:07 +0530

    create login api

--- 14_DAY/BACKEND/src/controllers/auth.controller.js
+++ 14_DAY/BACKEND/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+const { profile } = require("console");
 const userModel = require("../models/auth.model");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
@@ -53,4 +54,54 @@ async function authRegister(req, res) {
   }
 }
 
-module.exports = { authRegister };
+async function authLogin(req, res) {
+  const { username, email, password } = req.body;
+
+  try {
+    const user = await userModel.findOne({
+      $or: [{ username }, { email }],
+    });
+
+    if (!user) {
+      return res.status(401).json({
+        message: "Unauthorized ",
+      });
+    }
+
+    const hasPassword = await crypto
+      .createHash("sha256")
+      .update(password)
+      .digest("base64");
+
+    if (hasPassword !== user.password) {
+      return res.status(401).json({
+        message: "Invalide password",
+      });
+    }
+
+    const token = jwt.sign(
+      {
+        id: user._id,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: "1d" },
+    );
+
+    res.cookie("token", token);
+
+    return res.status(200).json({
+      message: "User logged In successfully",
+      user: {
+        id: user._id,
+        username: user.username,
+        email: user.email,
+        bio: user.bio,
+        profile: user.profile,
+      },
+    });
+  } catch (error) {
+    console.log(`Auth Login Error : ${error} `);
+  }
+}
+
+module.exports = { authRegister, authLogin };
