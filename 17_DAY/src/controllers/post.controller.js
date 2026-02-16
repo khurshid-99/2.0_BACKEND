8156f95afac8165cf46a5a8ef81f9b393451e63e
Khurshid Alam <khurshid1556@gmail.com>
2026-02-16 15:36:52 +0530

    all api is working

--- 17_DAY/src/controllers/post.controller.js
+++ 17_DAY/src/controllers/post.controller.js
@@ -11,7 +11,9 @@ async function createPostController(req, res) {
   const { token } = req.cookies;
   const file = req.file;
   const { caption } = req.body;
-  console.log(token);
+  //   console.log(token);
+
+  // console.log(file);
 
   if (!token) {
     return res.status(401).json({
@@ -48,4 +50,86 @@ async function createPostController(req, res) {
   });
 }
 
-module.exports = { createPostController };
+async function getPostController(req, res) {
+  const { token } = req.cookies;
+
+  if (!token) {
+    return res.status(401).json({
+      message: "Unauthorized Access. ",
+    });
+  }
+
+  let decoded = null;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Token Invalid",
+    });
+  }
+
+  const userId = decoded.id;
+
+  const posts = await postModel.find({
+    user: userId,
+  });
+
+  return res.status(200).json({
+    message: "Posts featch successfully",
+    posts,
+  });
+}
+
+async function getPostDetailsController(req, res) {
+  const { token } = req.cookies;
+  const { postId } = req.params;
+
+  // console.log(postId, "postid");
+
+  if (!token) {
+    return res.status(401).json({
+      message: "UnAuthorized Access",
+    });
+  }
+
+  let decoded = null;
+
+  try {
+    decoded = jwt.verify(token, process.env.JWT_SECRET);
+  } catch (error) {
+    return res.status(401).json({
+      message: "Unauthorized access!.",
+    });
+  }
+
+  const userId = decoded.id;
+  // const postId = req.params.postId;
+
+  const post = await postModel.findById(postId);
+
+  if (!post) {
+    return res.status(401).json({
+      message: "Post not found",
+    });
+  }
+
+  const isValidUser = post.user.toString() === userId;
+
+  if (!isValidUser) {
+    return res.status(403).json({
+      message: "Frobidden Contend",
+    });
+  }
+
+  return res.status(200).json({
+    message: "Post fetched successfully ",
+    post,
+  });
+}
+
+module.exports = {
+  createPostController,
+  getPostController,
+  getPostDetailsController,
+};
--- 17_DAY/src/routes/posts.route.js
+++ 17_DAY/src/routes/posts.route.js
@@ -1,5 +1,9 @@
 const express = require("express");
-const { createPostController } = require("../controllers/post.controller");
+const {
+  createPostController,
+  getPostController,
+  getPostDetailsController,
+} = require("../controllers/post.controller");
 const multer = require("multer");
 
 const upload = multer({ storage: multer.memoryStorage() });
@@ -7,5 +11,7 @@ const upload = multer({ storage: multer.memoryStorage() });
 const postRouter = express.Router();
 
 postRouter.post("/", upload.single("post"), createPostController);
+postRouter.get("/", getPostController);
+postRouter.get("/:postId", getPostDetailsController);
 
 module.exports = postRouter;
